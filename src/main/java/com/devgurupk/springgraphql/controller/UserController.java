ea5868d3f4131238dae89e514aaac95434ebee1b
Waqas Dilawar <waqasdilawardaha@gmail.com>
2022-05-20 14:59:18 +0500

    Implemented subscription for getting a random user

--- src/main/java/com/devgurupk/springgraphql/controller/UserController.java
+++ src/main/java/com/devgurupk/springgraphql/controller/UserController.java
@@ -4,8 +4,11 @@ import com.devgurupk.springgraphql.model.User;
 import com.devgurupk.springgraphql.repository.UserRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.List;
 
 @Controller
@@ -19,12 +22,25 @@ public class UserController
     }
 
     @QueryMapping
-    public List<User> allUsers(){
+    public List<User> allUsers()
+    {
         return userRepository.findAll();
     }
 
     @QueryMapping
-    public User findOne(@Argument Long id){
+    public User findOne(@Argument Long id)
+    {
         return userRepository.findById(id);
+    }
+
+    @SubscriptionMapping
+    public Flux<User> getRandomUser()
+    {
+        return Flux.interval(Duration.ofSeconds(1)).map(i -> {
+            long leftLimit = 1L;
+            long rightLimit = 3L;
+            long generatedLong = leftLimit + (long) (Math.random() * (rightLimit - leftLimit));
+            return userRepository.findById(generatedLong);
+        });
     }
 }
